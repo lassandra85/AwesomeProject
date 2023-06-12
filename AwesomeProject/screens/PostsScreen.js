717eba7a05af3311de1e9d3eb1b2b709a95c717c
lassandra85 <lassandra85@gmail.com>
2023-06-12 22:39:36 +0200

    screen

---
+++ AwesomeProject/screens/PostsScreen.js
@@ -0,0 +1,6 @@
+import {  
+  Text,  
+    View, 
+  StyleSheet,
+} from "react-native";
+import React from "react";
---
+++ AwesomeProject/screens/RegistrationScreen.js
@@ -0,0 +1,14 @@
+import {  
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
